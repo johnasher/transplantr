65bd30c10d1ac6bb297c697aef9605754067edab
John Asher <john.asher@outlook.com>
2019-12-11 15:00:46 +0000

    add UKKRI unit tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(transplantr)
+
+test_check("transplantr")
---
+++ tests/testthat/test_kri.R
@@ -0,0 +1,24 @@
+context("Kidney risk indices")
+library(transplantr)
+
+test_that("UKKRRI gives correct result", {
+  expect_equal(round(ukkrri(45, 0, 750, 0), 2), 0.61)
+  expect_equal(round(ukkrri(55, 1, 960, 1), 2), 1.34)
+})
+
+test_that("UKKDRI gives correct result", {
+  expect_equal(round(ukkdri(age = 50, height = 170, htn = 1, sex = "F", cmv = 0, gfr = 90, hdays = 2), 2), 0.86)
+})
+
+test_that("Watson UKKDRI is correct", {
+  expect_equal(round(watson_ukkdri(40, 0, 75, 0, 0), 2), 1.00)
+})
+
+test_that("USKDRI is correct", {
+  expect_equal(round(uskdri(age = 40, height = 170, weight = 80, eth = "non-black", htn = 0, dm = 0,
+                            cva = 0, creat = 120, hcv = 0, dcd = 0, units = "SI"), 3), 1.082)
+  expect_equal(round(uskdri(age = 40, height = 170, weight = 80, eth = "non-black", htn = 0, dm = 0,
+                            cva = 0, creat = 1.4, hcv = 0, dcd = 0, units = "US"), 3), 1.092)
+  expect_equal(round(uskdri(age = 52, height = 183, weight = 81, eth = "non-black", htn = 1, dm = 0,
+                            cva = 1, creat = 1.7, hcv = 0, dcd = 1, units = "US"), 3), 1.775)
+})
