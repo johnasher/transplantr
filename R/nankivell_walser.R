93f4bbccfbfe0e6bfbb6a7932674ebfebbee7744
John Asher <john.asher@outlook.com>
2020-02-27 15:36:54 +0000

    Nankivell and Walser corrected, documentation and tests complete

--- R/nankivell_walser.R
+++ R/nankivell_walser.R
@@ -11,7 +11,7 @@
 #' @param SCr numeric vector of serum creatinine in µmol/l (or mg/dl if units = "US")
 #' @param Urea numeric vector of serum urea in mmol/l (or BUN in mg/dl if units = "US")
 #' @param Weight numeric vector of patient weights in kilograms
-#' @param Height numeric vector of patient heights in metres
+#' @param Height numeric vector of patient heights in centimetres
 #' @param Sex character vector of sex ("F" for female, "M" for male)
 #' @param Units non-vectorised optional parameter for creatinine and urea/BUN units ("SI" for µmol/l (default), "US" for mg/dl)
 #'
@@ -19,21 +19,41 @@
 #' @export
 #'
 #' @examples
+#' nankivell(SCr = 118, Urea = 13.4, Weight = 65, Height = 172, Sex = "M") # 44.55
 nankivell = function(SCr, Urea, Weight, Height, Sex, Units = "SI") {
-  # convert creatinine to mg/dl
-  if (units == "SI") {
-    SCr = SCr / 88.4
+  # convert creatinine to mg/dl and BUN to urea
+  if (Units == "US") {
+    SCr = SCr * 88.4
+    Urea = bun_to_urea(Urea)
   }
 
   # conversion factor for Sex
   corr = ifelse(Sex == "M", 35, 25)
 
-  nvlGFR = 6.7 / SCr +
-          0.25 * Weight +
-          -0.5 * Urea - 100 +
-          -100 / Height ^ 2 +
-          corr
-  nvlGFR
+  6.7 / (SCr * 88.42) + 0.25 * Weight -0.5 * Urea -100 / Height ^ 2 + corr
+}
+
+#' eGFR using Nankivell formula (US units)
+#'
+#' A wrapper function for the nankivell() vectorised function to calculate eGFR using the Nankivell
+#' formula, but using creatinine and BUN in mg/dl rather than international units.
+#'
+#' Reference: Nankivell BJ, Gruenwald SM, Allen RD, Chapman JR: Predicting glomerular filtration rate
+#' after renal transplantation. Transplantation 1995; 59:1683-89.
+#'
+#' @param SCr numeric vector of serum creatinine in µmol/l (or mg/dl if units = "US")
+#' @param Urea numeric vector of serum urea in mmol/l (or BUN in mg/dl if units = "US")
+#' @param Weight numeric vector of patient weights in kilograms
+#' @param Height numeric vector of patient heights in centimetres
+#' @param Sex character vector of sex ("F" for female, "M" for male)
+#'
+#' @return a numeric vector of eGFR values
+#' @export
+#'
+#' @examples
+#' nankivell_US(SCr = 1.33, Urea = 13.4, Weight = 65, Height = 172, Sex = "M") # 44.55
+nankivell_US = function(SCr, Urea, Weight, Height, Sex, Units = "SI") {
+  nankivell(SCr, Urea, Weight, Height, Sex, Units = "US")
 }
 
 #' eGFR using the Nankivell-SPK formula
@@ -51,7 +71,7 @@ nankivell = function(SCr, Urea, Weight, Height, Sex, Units = "SI") {
 #' @param Age numeric vector of patient ages in years
 #' @param Sex character vector of sex ("F" for female, "M" for male)
 #' @param Weight numeric vector of patient weights in kilograms
-#' @param Height numeric vector of patient heights in metres
+#' @param Height numeric vector of patient heights in centimetres
 #' @param Units non-vectorised optional parameter for creatinine and urea/BUN units ("SI" for µmol/l (default), "US" for mg/dl)
 #' @param Offset non-vectorised optional numeric parameter for offset in years
 #'
@@ -59,9 +79,10 @@ nankivell = function(SCr, Urea, Weight, Height, Sex, Units = "SI") {
 #' @export
 #'
 #' @examples
+#' nankivell_spk(SCr = 118, Age = 74, Sex = "M", Weight = 172, Height = 172) # 48.8
 nankivell_spk = function(SCr, Age, Sex, Weight, Height, Units = "SI", Offset = 0) {
   # convert creatinine to umol/l
-  if (units == "US") {
+  if (Units == "US") {
     SCr = SCr * 88.4
   }
 
@@ -69,8 +90,35 @@ nankivell_spk = function(SCr, Age, Sex, Weight, Height, Units = "SI", Offset = 0
   corr = ifelse(Sex == "M", 71.4, 50.4)
 
   nvspkGFR = corr + 5520 / SCr + 0.27 * Weight -
-            0.50 * Age - 0.29 * height
+            0.50 * Age - 0.29 * Height
   nvspkGFR
+}
+
+#' eGFR using the Nankivell-SPK formula (US units)
+#'
+#' A wrapper function for the nankivell_spk() vectorised function to calculate the eGFR
+#' using the Nankivell-SPK formula using serum creatinine in mg/dl.
+#' To allow for serial measurements over time, such as
+#' for transplant follow-up data, there is an optional offset = n parameter which increases the age
+#' value used in the equation by n years.
+#'
+#' Reference: Nankivell BJ, Chapman JR, Allen RD: Predicting glomerular filtration rate after
+#' simultaneous pancreas and kidney transplantation. Clin Transplant 1995; 9(2): 129-134
+#'
+#' @param SCr numeric vector of serum creatinine in µmol/l (or mg/dl if units = "US")
+#' @param Age numeric vector of patient ages in years
+#' @param Sex character vector of sex ("F" for female, "M" for male)
+#' @param Weight numeric vector of patient weights in kilograms
+#' @param Height numeric vector of patient heights in metres
+#' @param Offset non-vectorised optional numeric parameter for offset in years
+#'
+#' @return numeric vector of eGFR values
+#' @export
+#'
+#' @examples
+#' nankivell_spk_US(SCr = 1.33, Age = 74, Sex = "M", Weight = 172, Height = 172) # 49.0
+nankivell_spk_US = function(SCr, Age, Sex, Weight, Height, Offset = 0) {
+  nankivell_spk(SCr, Age, Sex, Weight, Height, Offset, Units = "US")
 }
 
 #' eGFR using the Walser formula
@@ -78,6 +126,8 @@ nankivell_spk = function(SCr, Age, Sex, Weight, Height, Units = "SI", Offset = 0
 #' A vectorised function to calculate eGFR using the Walser formula. By default the equation accepts
 #' serum creatinine in µmol/l but can be changed to mg/dl by setting the units parameter to "US".
 #'
+#' Reference: Walser M, Drew HH, Guldan JL. Prediction of glomerular filtration rate in advanced
+#' chronic renal failure. Kidney International 1993; 44:2245-1148.
 #'
 #' @param SCr numeric vector of serum creatinine in µmol/l (or mg/dl if units = "US")
 #' @param Age numeric vector of patient ages in years
@@ -89,20 +139,40 @@ nankivell_spk = function(SCr, Age, Sex, Weight, Height, Units = "SI", Offset = 0
 #' @export
 #'
 #' @examples
+#' walser(SCr = 118, Age = 74, Weight = 65, Sex = "M") # 56.1
 walser = function(SCr, Age, Weight, Sex, Units = "SI") {
-  # convert creatinine to mg/dl
-  if (units == "SI") {
+  # convert creatinine from mg/dl
+  if (Units == "SI") {
     SCr = SCr / 88.4
   }
 
   #correction factors
-  walfage = ifelse(Sex == "M", 0.103, 0.08)
+  walfage = ifelse(Sex == "M", -0.103, -0.08)
   walfem = ifelse(Sex == "M", 7.57, 6.05)
   walwt = ifelse(Sex == "M", 0.096, 0.08)
-  walcorr = ifelse(Sex == "M", 6.66, 4.81)
+  walcorr = ifelse(Sex == "M", -6.66, -4.81)
 
-  wlsGFR = walfem / (SCr / 10) -
-            walfage * Age +
-            walwt * Weight - walcorr
-  wlsGFR
+  walfem / (SCr * 0.0884) + walfage * Age + walwt * Weight + walcorr
+}
+
+#' eGFR using the Walser formula (US units)
+#'
+#' A wrapper function for the walser() vectorised function to calculate eGFR using the Walser formula,
+#' using erum creatinine in mg/dl.
+#'
+#' Reference: Walser M, Drew HH, Guldan JL. Prediction of glomerular filtration rate in advanced
+#' chronic renal failure. Kidney International 1993; 44:2245-1148.
+#'
+#' @param SCr numeric vector of serum creatinine in µmol/l (or mg/dl if units = "US")
+#' @param Age numeric vector of patient ages in years
+#' @param Weight numeric vector of patient weights in kilograms
+#' @param Sex character vector of sex ("F" for female, "M" for male)
+#'
+#' @return numeric vectors of eGFR values
+#' @export
+#'
+#' @examples
+#' walser_US(SCr = 1.33, Age = 74, Weight = 65, Sex = "M") # 56.3
+walser_US = function(SCr, Age, Weight, Sex) {
+  walser(SCr, Age, Weight, Sex, Units = "US")
 }
--- tests/testthat/test_egfr.R
+++ tests/testthat/test_egfr.R
@@ -17,3 +17,21 @@ test_that("Cockcroft-Gault gives correct result", {
   expect_equal(round(cockcroft(creat = 88.4, age = 25, sex = "F", weight = 60), 2), 81.46)
 })
 
+test_that("Nankivell gives correct result", {
+  expect_equal(round(nankivell(SCr = 118, Urea = 13.4, Weight = 65, Height = 172, Sex = "M"), 1), 44.5)
+  expect_equal(round(nankivell(SCr = 1.33, Urea = 38, Weight = 65, Height = 172, Sex = "M", Units = "US"), 1), 44.5)
+  expect_equal(round(nankivell_US(SCr = 1.33, Urea = 38, Weight = 65, Height = 172, Sex = "M"), 1), 44.5)
+
+})
+
+test_that("Nankivell-SPK gives correct result", {
+  expect_equal(round(nankivell_spk(SCr = 118, Age = 74, Sex = "M", Weight = 65, Height = 172), 1), 48.8)
+  expect_equal(round(nankivell_spk(SCr = 1.33, Age = 74, Sex = "M", Weight = 65, Height = 172, Units = "US"), 1), 49.0)
+  expect_equal(round(nankivell_spk_US(SCr = 1.33, Age = 74, Sex = "M", Weight = 65, Height = 172), 1), 49.0)
+})
+
+test_that("Walser gives correct result", {
+  expect_equal(round(walser(SCr = 118, Age = 74, Weight = 65, Sex = "M"), 1), 56.1)
+  expect_equal(round(walser(SCr = 1.33, Age = 74, Weight = 65, Sex = "M", Units = "US"), 1), 56.3)
+  expect_equal(round(walser_US(SCr = 1.33, Age = 74, Weight = 65, Sex = "M"), 1), 56.3)
+})
