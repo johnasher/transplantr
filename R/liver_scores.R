cc525ab3184780efcfeebdb49dae7c29fcd1238d
John Asher <john.asher@outlook.com>
2020-02-27 14:16:04 +0000

    ET-DRI, DRI, SOFT, Pedi-SOFT Roxygen completed and unit tests complete

--- R/liver_scores.R
+++ R/liver_scores.R
@@ -5,74 +5,119 @@
 #' population. The American liver DRI is available using the transplantr::liver_dri() function.
 #'
 #' Reference: Braat AE, Blok JJ, Putter H, et al. The Eurotransplant Donor Risk Index in Liver
-#' Transplantation: ET-DRI. American Journal of Transplantation 2012; 12: 2789–2796.
+#' Transplantation: ET-DRI. American Journal of Transplantation 2012;12: 2789–2796.
 #'
-#' @param Age numeric vector of patient ages in years
-#' @param COD character string vector of donor causes of death, one of "anoxia", "cva" or "other"
-#' @param DCD numeric vector of whether DCD (1 = yes, 0 = no)
-#' @param Split numeric vector of whether liver split (1 = yes, 0 = no)
-#' @param Share character string vector of type of sharing, one of "regional" or "national"
-#' @param CIT numeric vector of cold ischaemic times in hours
-#' @param GTT numeric vector of last pre-transplant serum gamma-GT level in IU/l
-#' @param Rescue numeric vector of whether rescue transplant (1 = yes, 0 = no)
+#' @param age numeric vector of patient ages in years
+#' @param cod character string vector of donor causes of death, one of "trauma", "anoxia", "cva" or "other"
+#' @param dcd numeric vector of whether DCD (1 = yes, 0 = no)
+#' @param split numeric vector of whether liver split (1 = yes, 0 = no)
+#' @param share character string vector of type of sharing, one of "local", regional" or "national"
+#' @param cit numeric vector of cold ischaemic times in hours
+#' @param gtt numeric vector of last pre-transplant serum gamma-GT level in IU/l
+#' @param rescue numeric vector of whether rescue transplant (1 = yes, 0 = no)
 #'
 #' @return numeric vector of ET-DRI scores
 #' @export
 #'
 #' @examples
-et_dri <- function(Age, COD, DCD, Split, Share, CIT, GTT, Rescue) {
-	agevar = ifelse(Age < 40, 0,
-			ifelse(Age < 50, 0.154,
-			ifelse(Age < 60, 0.274,
-			ifelse(Age < 70, 0.424, 0.501))))
-	codvar = ifelse(COD == "anoxia", 0.079,
-			ifelse(COD == "cva", 0.145, 0.184))
-	dcdvar = 0.411 * DCD
-	splitvar = 0.422 * Split
-	sharevar = ifelse(Share == "regional", 0.105, 0.244)
-	citvar = 0.010 * (CIT - 8)
-	ggtvar = 0.06 * (GGT - 50) / 100
-	rescuevar = 0.180 * Rescue
+#' et_dri(age = 39, cod = "trauma", dcd = 0, split = 0, share = "local", cit = 8, ggt = 50, rescue = 0) # 1.00
+#' round(et_dri(age = 25, cod = "cva", dcd = 0, split = 0, share = "local", cit = 8, ggt = 50, rescue = 0) # 1.15
+et_dri <- function(age, cod, dcd, split, share, cit, ggt, rescue) {
+	agevar = ifelse(age < 40, 0,
+			ifelse(age < 50, 0.154,
+			ifelse(age < 60, 0.274,
+			ifelse(age < 70, 0.424, 0.501))))
+	codvar = ifelse(cod == "anoxia", 0.079,
+			ifelse(cod == "cva", 0.145, ifelse(cod == "trauma", 0, 0.184)))
+	dcdvar = 0.411 * dcd
+	splitvar = 0.422 * split
+	sharevar = ifelse(share == "local", 0, ifelse(share == "regional", 0.105, 0.244))
+	citvar = 0.010 * (cit - 8)
+	ggtvar = (ggt - 50) / 100
+	rescuevar = 0.180 * rescue
 
-	exp(agevar + codvar + dcdvar + splitvar + sharevar +
-			citvar + ggtvar + rescuevar)
+	exp(0.960 * (agevar + codvar + citvar + sharevar) + dcdvar + splitvar + 0.06 * ggtvar + rescuevar)
 }
 
-#' Title
+#' Liver Donor Risk Index (DRI)
 #'
-#' @param Age numeric vector of patient ages in years
-#' @param COD character string vector of donor causes of death, one of "anoxia", "cva" or "other"
-#' @param Race
-#' @param DCD numeric vector of whether DCD (1 = yes, 0 = no)
-#' @param Split numeric vector of whether liver split (1 = yes, 0 = no)
-#' @param Share character string vector of type of sharing, one of "regional" or "national"
-#' @param CIT numeric vector of cold ischaemic times in hours
-#' @param Height
+#' A vectorised function to calculate the Liver Donor Risk Index as published by Feng and others.
 #'
-#' @return
+#' Reference: Feng S, Goodrich NP, Bragg-Gresham JL et al. Characteristics Associated with
+#' Liver Graft Failre: The Concept of a Donor Risk Index.
+#' American Journal of Transplantation 2006; 6:783-790.
+#'
+#' @param age numeric vector of patient ages in years
+#' @param cod character string vector of donor causes of death, one of "trauma", "anoxia", "cva" or "other"
+#' @param eth character string vector of ethnicity, one of "black", "white" or "other"
+#' @param dcd numeric vector of whether DCD (1 = yes, 0 = no)
+#' @param split numeric vector of whether liver split (1 = yes, 0 = no)
+#' @param share character string vector of type of sharing, one of "regional" or "national"
+#' @param cit numeric vector of cold ischaemic times in hours
+#' @param height numeric vector of patient heights in cm
+#'
+#' @return numeric vector of liver DRI values
 #' @export
 #'
 #' @examples
-liver_dri <- function(Age, COD, Race, DCD, Split, Share, CIT, Height) {
-	agevar = ifelse(Age < 40, 0,
-			ifelse(Age < 50, 0.154,
-			ifelse(Age < 60, 0.274,
-			ifelse(Age < 70, 0.424, 0.501))))
-	codvar = ifelse(COD == "anoxia", 0.079,
-			ifelse(COD == "cva", 0.145, 0.184))
-	racevar = ifelse(Race == "black", 0.176, 0.126)
-	dcdvar = 0.411 * DCD
-	splitvar = 0.422 * Split
-	sharevar = ifelse(Share == "regional", 0.105, 0.244)
-	citvar = 0.010 * CIT
+#' liver_dri(age = 25, cod = "trauma", eth = "white", dcd = 0, split = 0, share = "local", cit = 8, height = 170) # 1.00
+#' liver_dri(age = 64, cod = "cva", eth = "white", dcd = 0, split = 0, share = "local", cit = 14, height = 170) # 1.88
+liver_dri <- function(age, cod, eth, dcd, split, share, cit, height) {
+	agevar = ifelse(age < 40, 0,
+			        ifelse(age < 50, 0.154,
+			        ifelse(age < 60, 0.274,
+			        ifelse(age < 70, 0.424, 0.501))))
+	codvar = ifelse(cod == "anoxia", 0.079,
+			        ifelse(cod == "cva", 0.145, ifelse(cod == "trauma", 0, 0.184)))
+	racevar = ifelse(eth == "black", 0.176, ifelse(eth == "white", 0, 0.126))
+	dcdvar = 0.411 * dcd
+	splitvar = 0.422 * split
+	sharevar = ifelse(share == "local", 0, ifelse(share == "regional", 0.105, 0.244))
+	citvar = 0.010 * (cit - 8)
 	heightvar = 0.066 * (170 - height) / 10
+
+	exp(agevar + codvar + racevar + dcdvar + splitvar + citvar + heightvar)
 }
 
-#P-SOFT score
-p_soft <- function(Age, BMI, PrevTx, AbdoSurg, Albumin, Dx, ICU, Admitted, MELD, LifeSupport, Encephalopathy, PVThrombosis, Ascites) {
+#' P-SOFT Score
+#'
+#' A vectorised function to calculate the pre-procurement component of the SOFT score used to
+#' predict patient survival after liver transplantation. The function needs the MELD score as one of
+#' its inputs - this is available using the transplantr::meld() function.
+#' The units for albumin are g/l but this
+#' is changed to g/dl if the optional Units parameter is set to "US"
+#'
+#' Reference: Rana A, Hardy MA, Halazun KJ, et al. Survival Outcomes Following Liver Transplantation
+#' (SOFT) Score: A Novel Method to Predict Patient Survival Following Liver Transplantation.
+#' American Journal of Transplantation 2008; 8:2537-2546.
+#'
+#' @param Age numeric vector of patient ages in years
+#' @param BMI numeric vector of patient BMI in kg/m2
+#' @param PrevTx numeric vector of number of previous transplants
+#' @param AbdoSurg numeric vector of whether previous abdominal surgery (1 = "yes", 0 = "no")
+#' @param Albumin numeric vector of serum albumin in g/l
+#' @param Dx numeric vector of whether on dialysis before transplant (1 = "yes", 0 = "no")
+#' @param ICU numeric vector of whether patients in intensive care unit before transplant (1 = "yes", 0 = "no")
+#' @param Admitted numeric vector of whether admitted to hospital pre-transplant (1 = "yes", 0 = "no")
+#' @param MELD numeric vector of MELD scores
+#' @param LifeSupport numeric vector of whether on life support pre-transplant (1 = "yes", 0 = "no")
+#' @param Encephalopathy numeric vector of whether encephalopathy present (1 = "yes", 0 = "no")
+#' @param PVThrombosis numeric vector of whether portal vein thrombosis (1 = "yes", 0 = "no")
+#' @param Ascites numeric vector of whether ascites pre-transplant (1 = "yes", 0 = "no")
+#' @param Units optional scalar for albumin units (one of "SI" for g/l, "US" for g/dl)
+#'
+#' @return numeric vector of P-SOFT scores
+#' @export
+#'
+#' @examples
+#' p_soft(Age = 65, BMI = 36, PrevTx = 2, AbdoSurg = 1, Albumin = 29, Dx = 0, ICU = 0, Admitted = 1, MELD = 32, LifeSupport = 0, Encephalopathy = 1, PVThrombosis = 1, Ascites = 1) # 37
+p_soft <- function(Age, BMI, PrevTx, AbdoSurg, Albumin, Dx, ICU, Admitted, MELD, LifeSupport, Encephalopathy, PVThrombosis, Ascites, Units = "SI") {
 	agepts = ifelse(Age > 60, 4, 0)
 	bmipts = ifelse(BMI > 35, 2, 0)
 	txpts = ifelse(PrevTx == 0, 0, ifelse(PrevTx == 1, 9, 14))
+	if (Units == "SI") {
+	  Albumin = Albumin / 10
+	  }
 	albpts = ifelse(Albumin < 2.0, 2, 0)
 	meldpts = ifelse(MELD > 30, 4, 0)
 	psoft = agepts + bmipts + txpts + albpts + meldpts +
@@ -81,28 +126,120 @@ p_soft <- function(Age, BMI, PrevTx, AbdoSurg, Albumin, Dx, ICU, Admitted, MELD,
 			5 * PVThrombosis + 3 * Ascites
 }
 
-# SOFT score from P-SOFT
-soft2 <- function(PSoft, PortalBleed, DonorAge, DonorCVA, DonorSCr, National, CIT) {
+#' SOFT score from P-SOFT
+#'
+#' A vectorised function to calculate SOFT Scores for predicting patient survival after liver
+#' transplantation when the P-SOFT score is already known. The P-SOFT Score can be calculated using
+#' the transplantr::p_soft() function. Alternatively, the SOFT Score can be calculated in full,
+#' including the P-SOFT parameters using the transplantr::soft() function. The units for donor serum
+#' creatinine are in µmol/l but can be changed to mg/dl by setting the Units parameter to "US".
+#'
+#' Reference: Rana A, Hardy MA, Halazun KJ, et al. Survival Outcomes Following Liver Transplantation
+#' (SOFT) Score: A Novel Method to Predict Patient Survival Following Liver Transplantation.
+#' American Journal of Transplantation 2008; 8:2537-2546.
+#'
+#' @param PSoft numeric vector of P-SOFT scores
+#' @param PortalBleed numeric vector of whether portal bleedn in 48 hours pre-transplant (1 = "yes", 0 = "no")
+#' @param DonorAge numeric vector of donor ages in years
+#' @param DonorCVA numeric vector of whether donor cause of death is CVA/stroke (1 = "yes", 0 = "no")
+#' @param DonorSCr numeric vector of donor terminal serum creatinine
+#' @param National numeric vector of whether national allocation (1 = "yes", 0 = "no")
+#' @param CIT numeric vector of cold ischaemic time in hours
+#' @param Units units to use for creatinine, "SI" (default) for µmol/l, "US" for mg/dl
+#'
+#' @return numeric vector of SOFT Scores
+#' @export
+#'
+#' @examples
+#' soft2(PSoft = 4, PortalBleed = 0, DonorAge = 61, DonorCVA = 1, DonorSCr = 140, National = 1, CIT = 12) # 13
+soft2 <- function(PSoft, PortalBleed, DonorAge, DonorCVA, DonorSCr, National, CIT, Units = "SI") {
 	dagepts = ifelse(DonorAge >= 10 && DonorAge <= 20, -2,
 			ifelse(DonorAge > 60, 3, 0))
+	if (Units == "SI") {
+	  DonorSCr = DonorSCr / 88.4
+	}
 	dcrpts = ifelse(DonorSCr > 1.5, 2, 0)
 	citpts = ifelse(CIT < 6, -3, 0)
 	PSoft + 6 * PortalBleed + dagepts + 2 * DonorCVA + dcrpts +
 		2 * National + citpts
 }
 
-# SOFT score
+#' SOFT score (Survival Outcomes Following Liver Transplantation)
+#'
+#' A vectorised function to calculate SOFT Scores for predicting patient survival after liver
+#' transplantation The units for donor serum creatinine are in µmol/l
+#' and recipient serum albumin in g/l but they can be changed to mg/dl and g/dl respectively
+#' by setting the Units parameter to "US".
+#'
+#' Reference: Rana A, Hardy MA, Halazun KJ, et al. Survival Outcomes Following Liver Transplantation
+#' (SOFT) Score: A Novel Method to Predict Patient Survival Following Liver Transplantation.
+#' American Journal of Transplantation 2008; 8:2537-2546.
+#'
+#' @param Age numeric vector of patient ages in years
+#' @param BMI numeric vector of patient BMI in kg/m2
+#' @param PrevTx numeric vector of number of previous transplants
+#' @param AbdoSurg numeric vector of whether previous abdominal surgery (1 = "yes", 0 = "no")
+#' @param Albumin numeric vector of serum albumin in g/l
+#' @param Dx numeric vector of whether on dialysis before transplant (1 = "yes", 0 = "no")
+#' @param ICU numeric vector of whether patients in intensive care unit before transplant (1 = "yes", 0 = "no")
+#' @param Admitted numeric vector of whether admitted to hospital pre-transplant (1 = "yes", 0 = "no")
+#' @param MELD numeric vector of MELD scores
+#' @param LifeSupport numeric vector of whether on life support pre-transplant (1 = "yes", 0 = "no")
+#' @param Encephalopathy numeric vector of whether encephalopathy present (1 = "yes", 0 = "no")
+#' @param PVThrombosis numeric vector of whether portal vein thrombosis (1 = "yes", 0 = "no")
+#' @param Ascites numeric vector of whether ascites pre-transplant (1 = "yes", 0 = "no")
+#' @param PSoft numeric vector of P-SOFT scores
+#' @param PortalBleed numeric vector of whether portal bleedn in 48 hours pre-transplant (1 = "yes", 0 = "no")
+#' @param DonorAge numeric vector of donor ages in years
+#' @param DonorCVA numeric vector of whether donor cause of death is CVA/stroke (1 = "yes", 0 = "no")
+#' @param DonorSCr numeric vector of donor terminal serum creatinine
+#' @param National numeric vector of whether national allocation (1 = "yes", 0 = "no")
+#' @param CIT numeric vector of cold ischaemic time in hours
+#' @param Units units to use for creatinine and albumin, "SI" (default) for µmol/l and g/l, "US" for mg/dl and g/dl
+#'
+#' @return numeric vector of SOFT Scores
+#' @export
+#'
+#' @examples
+#' soft(Age = 35, BMI = 20, PrevTx = 0, AbdoSurg = 1, Albumin = 30, Dx = 0, ICU = 0, Admitted = 0, MELD = 29, LifeSupport = 0, Encephalopathy = 1, PVThrombosis = 0, Ascites = 1, PortalBleed = 0, DonorAge = 44, DonorCVA = 0, DonorSCr = 110, National = 0, CIT = 8) # 7
 soft <- function(Age, BMI, PrevTx, AbdoSurg, Albumin, Dx, ICU, Admitted,
 			MELD, LifeSupport, Encephalopathy, PVThrombosis, Ascites,
-			PortalBleed, DonorAge, DonorCVA, DonorSCr, National, CIT) {
+			PortalBleed, DonorAge, DonorCVA, DonorSCr, National, CIT, Units = "SI") {
+  # convert units if needed
+  if (Units == "SI") {
+    Albumin = Albumin / 10
+    DonorSCr = DonorSCr / 88.4
+  }
+  # calculate P-SOFT
 	Psoft = p_soft(Age, BMI, PrevTx, AbdoSurg, Albumin, Dx, ICU, Admitted,
 			MELD, LifeSupport, Encephalopathy, PVThrombosis, Ascites)
+	# calculate SOFT
 	soft2(Psoft, PortalBleed, DonorAge, DonorCVA, DonorSCr, National, CIT)
 }
 
-# Pedi-SOFT
+#' Pedi-SOFT Score
+#'
+#' A vectorised function to calculate the Pedi-SOFT score used to predict survival after
+#' liver transplantation in children.
+#'
+#' Reference: Rana A, Pallister ZS, Guiteau JJ, et al.
+#' Survival Outcomes Following Pediatric Liver Transpantation (Pedi-SOFT) Score:
+#' A Novel Predictive Index.
+#' American Journal of Transplantation 2015; 15:1855-1863.
+#'
+#' @param CTVG numeric vector of whether cadaveric technical variant graft (1 for "yes", 0 for "no")
+#' @param Weight numeric vector of recipient weight in kg
+#' @param Dx numeric vector of whether on dialysis or creatinine clearance under 30 (1 for "yes", 0 for "no")
+#' @param LifeSupport numeric vector of whether on life support pre-transplant (1 for "yes", 0 for "no")
+#' @param PrevTx numeric vector of number of previous liver transplants
+#'
+#' @return numeric vector of Pedi-SOFT scores
+#' @export
+#'
+#' @examples
+#' pedi_soft(CTVG = 1, Weight = 10, Dx = 0, LifeSupport = 0, PrevTx = 0) # 4
 pedi_soft = function(CTVG, Weight, Dx, LifeSupport, PrevTx) {
-	weightpts = ifelse(Weight < 6, 4, 0)
+	weightpts = ifelse(Weight < 6.0, 6, 0)
 	txpts = ifelse(PrevTx == 0, 0, ifelse(PrevTx == 1, 15, 49))
 	weightpts + txpts + 4 * CTVG + 17 * Dx + 27 * LifeSupport
 }
--- tests/testthat/test_liver.R
+++ tests/testthat/test_liver.R
@@ -21,3 +21,37 @@ test_that("APRI gives correct result", {
   expect_equal(round(apri(ast = 38, plt = 150, ast_uln = 40), 2), 0.63)
   expect_equal(round(apri(ast = 160, plt = 75), 2), 5.33)
 })
+
+test_that("ET-DRI gives correct result", {
+  expect_equal(round(et_dri(age = 39, cod = "trauma", dcd = 0, split = 0, share = "local", cit = 8, ggt = 50, rescue = 0), 2), 1.00)
+  expect_equal(round(et_dri(age = 65, cod = "trauma", dcd = 0, split = 0, share = "local", cit = 8, ggt = 50, rescue = 0), 2), 1.50)
+  expect_equal(round(et_dri(age = 65, cod = "trauma", dcd = 0, split = 0, share = "local", cit = 14, ggt = 50, rescue = 0), 2), 1.59)
+  expect_equal(round(et_dri(age = 25, cod = "trauma", dcd = 0, split = 0, share = "local", cit = 8, ggt = 200, rescue = 0), 2), 1.09)
+  expect_equal(round(et_dri(age = 25, cod = "trauma", dcd = 0, split = 0, share = "local", cit = 12, ggt = 50, rescue = 1), 2), 1.24)
+  expect_equal(round(et_dri(age = 25, cod = "trauma", dcd = 1, split = 0, share = "local", cit = 8, ggt = 50, rescue = 0), 2), 1.51)
+  expect_equal(round(et_dri(age = 25, cod = "cva", dcd = 0, split = 0, share = "local", cit = 8, ggt = 50, rescue = 0), 2), 1.15)
+  expect_equal(round(et_dri(age = 25, cod = "trauma", dcd = 0, split = 1, share = "regional", cit = 8, ggt = 50, rescue = 0), 2), 1.69)
+})
+
+test_that("Liver DRI gives correct result", {
+  expect_equal(round(liver_dri(age = 25, cod = "trauma", eth = "white", dcd = 0, split = 0, share = "local", cit = 8, height = 170), 2), 1.00)
+  expect_equal(round(liver_dri(age = 64, cod = "trauma", eth = "white", dcd = 0, split = 0, share = "local", cit = 8, height = 170), 2), 1.53)
+  expect_equal(round(liver_dri(age = 64, cod = "cva", eth = "white", dcd = 0, split = 0, share = "local", cit = 8, height = 170), 2), 1.77)
+  expect_equal(round(liver_dri(age = 64, cod = "cva", eth = "white", dcd = 0, split = 0, share = "local", cit = 14, height = 170), 2), 1.88)
+  expect_equal(round(liver_dri(age = 25, cod = "trauma", eth = "white", dcd = 0, split = 0, share = "local", cit = 14, height = 170), 2), 1.06)
+  expect_equal(round(liver_dri(age = 25, cod = "trauma", eth = "white", dcd = 1, split = 0, share = "local", cit = 14, height = 170), 2), 1.60)
+})
+
+test_that("SOFT scores give correct results", {
+  expect_equal(soft(Age = 35, BMI = 20, PrevTx = 0, AbdoSurg = 1, Albumin = 30, Dx = 0, ICU = 0, Admitted = 0, MELD = 29, LifeSupport = 0, Encephalopathy = 1, PVThrombosis = 0, Ascites = 1, PortalBleed = 0, DonorAge = 44, DonorCVA = 0, DonorSCr = 110, National = 0, CIT = 8), 7)
+  expect_equal(p_soft(Age = 65, BMI = 36, PrevTx = 2, AbdoSurg = 1, Albumin = 29, Dx = 0, ICU = 0, Admitted = 1, MELD = 32, LifeSupport = 0, Encephalopathy = 1, PVThrombosis = 1, Ascites = 1), 37)
+  expect_equal(soft2(PSoft = 4, PortalBleed = 0, DonorAge = 61, DonorCVA = 1, DonorSCr = 140, National = 1, CIT = 12), 13)
+})
+
+test_that("Pedi-SOFT score gives correct results", {
+  expect_equal(pedi_soft(CTVG = 0, Weight = 31.5, Dx = 0, LifeSupport = 0, PrevTx = 0), 0)
+  expect_equal(pedi_soft(CTVG = 0, Weight = 17.6, Dx = 1, LifeSupport = 0, PrevTx = 0), 17)
+  expect_equal(pedi_soft(CTVG = 1, Weight = 4.9, Dx = 0, LifeSupport = 0, PrevTx = 1), 25)
+  expect_equal(pedi_soft(CTVG = 1, Weight = 5.4, Dx = 1, LifeSupport = 0, PrevTx = 0), 27)
+  expect_equal(pedi_soft(CTVG = 1, Weight = 5.9, Dx = 0, LifeSupport = 1, PrevTx = 1), 52)
+})
